978f52602b13973381a4dae4d2507e592ec68de4
Davide Albano <dalbano@1-B-27.42heilbronn.de>
2024-10-08 16:43:46 +0200

    added overflow case memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,37 +6,46 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:31:23 by dalbano           #+#    #+#             */
-/*   Updated: 2024/10/08 12:43:46 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/10/08 16:42:53 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	*memmove(void *dest_str, const void *src_str, size_t numBytes)
+void	*ft_memmove(void *dest_str, const void *src_str, size_t numBytes)
 {
 	size_t		temp;
 	char		*dest;
 	const char	*src;
 
-	temp = -1;
 	dest = (char *)dest_str;
 	src = (const char *)src_str;
-	while (++temp < numBytes)
+	if (dest == src)
+		return (dest_str);
+	if (dest > src && dest < src + numBytes)
 	{
-		dest[temp] = src[temp];
+		temp = numBytes;
+		while (temp > 0)
+		{
+			temp--;
+			dest[temp] = src[temp];
+		}
+	}
+	else
+	{
+		temp = -1;
+		while (++temp < numBytes)
+			dest[temp] = src[temp];
 	}
 	return (dest_str);
 }
 
-// #include <stdio.h>
-// #include <string.h>
-
-// int main () 
-// {
-//    char dest_str[] = "oldstring";
-//    const char src_str[]  = "newstring";
-//    printf("Before memmove dest = %s, src = %s\n", dest_str, src_str);
-//    memmove(dest_str, src_str, 9);
-//    printf("After memmove dest = %s, src = %s\n", dest_str, src_str);
-//    return(0);
-// }
+int main()
+{
+    char dest_str[] = "oldstring";
+    const char src_str[] = "newstring";
+    printf("Before memmove dest = %s, src = %s\n", dest_str, src_str);
+    ft_memmove(dest_str, src_str, 9);
+    printf("After memmove dest = %s, src = %s\n", dest_str, src_str);
+    return 0;
+}
