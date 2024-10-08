07f0b31e8fc7484ba007cde028b80584b042364e
Davide Albano <dalbano@1-B-27.42heilbronn.de>
2024-10-08 09:38:00 +0200

    added isdigit

---
+++ ft_isdigit.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 09:32:30 by dalbano           #+#    #+#             */
+/*   Updated: 2024/10/08 09:37:40 by dalbano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+// #include <stdio.h>
+// #include <ctype.h>
+// int main(void)
+// {
+// 	int c = '0';
+// 	for(c = ','; c < '<'; c++)
+// 	{
+// 		printf("%d %d\n", ft_isdigit(c), c);
+// 		printf("%d %d\n\n", isdigit(c), c);
+// 	}
+// 	return 0;
+// }
