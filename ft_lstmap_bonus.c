85a05f5b6a9ff458f17ff74e42c28f7e82c4b942
Davide Albano <dalbano@1-D-32.42heilbronn.de>
2024-10-14 13:24:39 +0200

    fixed lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 14:22:07 by dalbano           #+#    #+#             */
-/*   Updated: 2024/10/13 12:07:21 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/10/14 13:23:38 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst != NULL)
 	{
 		new_content = f(lst->content);
-		new_node = ft_lstnew(new_content);
-		if (new_node == NULL)
+		if (!new_content)
 		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		new_node = ft_lstnew(new_content);
+		if (!new_node)
+		{
+			del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
