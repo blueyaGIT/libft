dcf69340246b9cba745cb8c43e680ec78fa80cc9
blueyaGIT <albanodavide50@gmail.com>
2025-04-03 11:10:56 +0200

    up

--- srcs/ft_free_arr.c
+++ srcs/ft_free_arr.c
@@ -6,29 +6,29 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 17:26:27 by dalbano           #+#    #+#             */
-/*   Updated: 2025/04/03 11:08:54 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/04/03 11:10:11 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 /**
- * @brief Frees a string array.
- * @param arr The string array to free.
+ * @brief Frees a string array and sets it to NULL.
+ * @param arr The address of the string array to free.
  */
-void	ft_free_arr(char **arr)
+void	ft_free_arr(char ***arr)
 {
 	int	i;
 
-	if (!arr || !*arr)
+	if (!arr || !*arr) // Check if arr or *arr is NULL
 		return;
+
 	i = 0;
 	while ((*arr)[i])
 	{
 		free((*arr)[i]);
-		(*arr)[i] = NULL;
+		(*arr)[i] = NULL;  // Properly NULLify the freed element
 		i++;
 	}
 	free(*arr);
-	*arr = NULL;
+	*arr = NULL;  // Prevent use-after-free
 }
