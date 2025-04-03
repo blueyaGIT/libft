86635d59ab1f1fdbcc799da9f94312f2f26a2f4b
blueyaGIT <albanodavide50@gmail.com>
2025-04-03 11:24:46 +0200

    up

--- srcs/ft_free_ptr.c
+++ srcs/ft_free_ptr.c
@@ -6,17 +6,17 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 13:36:59 by dalbano           #+#    #+#             */
-/*   Updated: 2025/04/03 11:04:32 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/04/03 11:24:34 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_free_ptr(void *temp)
+void	ft_free_ptr(void *ptr)
 {
-	if (temp)
+	if (ptr)
 	{
-		free(temp);
-		temp = NULL;
+		free(ptr);
+		ptr = NULL;
 	}
 }
