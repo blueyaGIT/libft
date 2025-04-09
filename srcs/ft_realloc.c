f146ce867ff4fea61f9d19ea6b68841ee6c3a69d
blueyaGIT <albanodavide50@gmail.com>
2025-04-09 09:59:54 +0200

    updated ft_realloc.c to ensure ubuntu compatibility

--- srcs/ft_realloc.c
+++ srcs/ft_realloc.c
@@ -6,11 +6,24 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 15:59:16 by dalbano           #+#    #+#             */
-/*   Updated: 2025/04/01 16:07:53 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/04/09 09:58:44 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	helper_func(size_t old_size, size_t new_size, size_t *copy_size)
+{
+    if (old_size < new_size)
+    {
+		*copy_size = old_size;
+	}
+    else
+    {
+		*copy_size = new_size;
+	}
+}
+
 
 /*
  * ft_realloc manually resizes the memory block pointed by ptr to size bytes.
@@ -27,21 +40,21 @@ void    *ft_realloc(void *ptr, size_t size)
     size_t  copy_size;
 
     if (!ptr)
-        return (malloc(size));
+	{
+		return (malloc(size));
+	}
     if (size == 0)
     {
         free(ptr);
         return (NULL);
     }
-
     old_size = *((size_t *)ptr - 1);
     new_ptr = malloc(size);
     if (!new_ptr)
-        return (NULL);
-	if (old_size < size)
-        copy_size = old_size;
-    else
-        copy_size = size;
+	{
+		return (NULL);
+	}
+	helper_func(old_size, size, &copy_size);
     ft_memcpy(new_ptr, ptr, copy_size);
     free(ptr);
     return (new_ptr);
